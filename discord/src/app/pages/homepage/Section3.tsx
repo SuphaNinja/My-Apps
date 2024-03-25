d5fa52f64155fe06b9b0292180fce9581e608348
Sid <sidricobjork@gmail.com>
2024-03-25 23:06:45 +0100

    added

--- discord/src/app/pages/homepage/Section3.tsx
+++ discord/src/app/pages/homepage/Section3.tsx
@@ -1,9 +1,22 @@
+"use client"
 
+import { useQuery } from "@tanstack/react-query";
+import api from "@/lib/axios";
 
 export default function Section3() {
+
+
+    const user = useQuery({
+        queryKey: ["user"],
+        queryFn: () => api.getCurrentUser(),
+    })
+
+
+    if (user.isLoading) return <div>Loading...</div>
     return (
         <div>
-            <h1>section 3</h1>
+            <h1>{user.data.name}</h1>
+            <button onClick={()=> console.log(user)}> test</button>
         </div>
     )
 }
